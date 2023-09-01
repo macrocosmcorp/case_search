87141981335c0e9947252245aa68f81517d71bbc
Pasha Khomchenko <pavel.kh005@gmail.com>
2023-09-01 18:43:27 -0400

    transition flask to pinecone and supabase

--- main.py
+++ main.py
@@ -2,23 +2,22 @@ import os
 import dotenv
 import cohere
 from flask import Flask, request, jsonify
-import hnswlib
-import pandas as pd
+import pinecone
+from supabase import create_client, Client
+
+dotenv.load_dotenv()
 
 app = Flask(__name__)
 
-# Cohere initialization
-dotenv.load_dotenv()
+# Cohere, Pinecone, and Supabase initialization
 co = cohere.Client(os.getenv('COHERE_API_KEY'))
-
-# Load the parquet file
-df = pd.read_parquet('embeddings/scotus_opinions.parquet')
-doc_embs = df['embedding'].tolist()
-
-# Load the hnswlib index
-index = hnswlib.Index(space='ip', dim=4096)
-index.init_index(max_elements=len(doc_embs), ef_construction=512, M=64)
-index.add_items(doc_embs, list(range(len(doc_embs))))
+pinecone.init(
+    api_key=os.getenv('PINECONE_API_KEY'),
+    environment='us-west1-gcp-free'
+)
+index = pinecone.Index('casearch')
+supabase: Client = create_client(os.environ.get(
+    "SUPABASE_URL"), os.environ.get("SUPABASE_KEY"))
 
 
 @app.route('/search', methods=['POST'])
@@ -31,15 +30,12 @@ def search():
 
     # Perform the vector search
     query_emb = co.embed(texts=[query], model="embed-english-v2.0").embeddings
-    doc_ids = index.knn_query(query_emb, k=100)[0][0]
+    query_response = index.query(top_k=100, vector=query_emb)
+    ids = [int(match["id"]) for match in query_response["matches"]]
 
-    # Get the top 100 results from the parquet file
-    result_keys = ['author_name', 'category', 'per_curiam',
-                   'case_name', 'date_filed', 'text', 'absolute_url']
-    results = [
-        {**df.loc[doc_id, result_keys].to_dict(), 'id': int(doc_id)}
-        for doc_id in doc_ids
-    ]
+    # Get the top 100 results from the Supabase
+    response = supabase.table('case_data').select("*").in_('id', ids).execute()
+    results = response.data
 
     return jsonify(results), 200
 
@@ -53,19 +49,16 @@ def similar():
         return jsonify({'error': 'Invalid ID'}), 400
 
     # Perform the vector search
-    doc_emb = df.loc[doc_id, 'embedding']
-    doc_ids = index.knn_query(doc_emb, k=100)[0][0]
+    query_response = index.query(top_k=100, id=str(doc_id))
+    ids = [int(match["id"]) for match in query_response["matches"]
+           if int(match["id"]) != doc_id]
 
-    # Get the top 100 results from the parquet file
-    result_keys = ['author_name', 'category', 'per_curiam',
-                   'case_name', 'date_filed', 'text', 'absolute_url']
-    results = [
-        {**df.loc[d_id, result_keys].to_dict(), 'id': int(d_id)}
-        for d_id in doc_ids if int(d_id) != doc_id
-    ]
+    # Get the top 100 results from the Supabase
+    response = supabase.table('case_data').select("*").in_('id', ids).execute()
+    results = response.data
 
     return jsonify(results), 200
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
